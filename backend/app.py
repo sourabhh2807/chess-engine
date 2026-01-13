052f10e35e0529426074dbecf30df57e49303218
Roshita Ailawadi <115929185+roshita05@users.noreply.github.com>
2026-01-13 06:20:50 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -28,6 +28,7 @@ def move():
 import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.10000", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 
 
